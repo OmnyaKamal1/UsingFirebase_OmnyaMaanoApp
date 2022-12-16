1be5b92b4ed1c926c10df5b0c1bdc95a1268bc1e
omnya <omnyakamal1@icloud.com>
2022-12-16 14:14:19 +0300

    Initial Commit

---
+++ UsingFirebase_OmnyaMaanoApp/UsingFirebase_OmnyaMaanoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  UsingFirebase_OmnyaMaanoAppApp.swift
+//  UsingFirebase_OmnyaMaanoApp
+//
+//  Created by Omnya Kamal  on 16/12/2022.
+//
+
+import SwiftUI
+
+@main
+struct UsingFirebase_OmnyaMaanoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
